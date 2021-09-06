4782ec6d8225e62c3b63cdb7c3158d4bf0b14a08
Blacklinkker <blacklinkker@gmail.com>
2021-09-06 16:35:22 -0300

    Completed app

---
+++ src/components/TaskItem.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
+
+const TaskItem = (props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.indexContainer}>
+                <Text style={styles.index}>{props.index}</Text>
+            </View>
+            <View style={styles.taskContainer}>
+                <Text style={styles.task}>{props.task}</Text>
+                <TouchableOpacity onPress={() => props.deleteTask()}>
+                    <MaterialIcons style={styles.delete} name="delete" size={18} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginHorizontal: '20',
+    },
+    indexContainer: {
+        backgroundColor: '#098DD6',
+        borderRadius: 12,
+        marginRight: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+    },
+    index: {
+        color: '#fff',
+        fontSize: 20,
+    },
+    taskContainer: {
+        backgroundColor: '#098DD6',
+        borderRadius: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1,
+        paddingHorizontal: 10, 
+        paddingVertical: 5, 
+        minHeight: '50'
+    },
+    task: {
+        color: '#fff',
+        width: '90%',
+        fontSize: 10,
+    },
+    delete: {
+        marginLeft: 10,
+    }
+
+}); 
+
+export default TaskItem;
